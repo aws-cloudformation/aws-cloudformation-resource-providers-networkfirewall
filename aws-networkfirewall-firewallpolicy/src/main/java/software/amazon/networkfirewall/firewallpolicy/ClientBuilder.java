589421720ea0fafd9b0bc5e20a18ec348f634d54
Daniel Takata <dttakata@amazon.com>
2022-02-14 16:43:20 -0600

    specify fips endpoints for PDT,OSU

--- aws-networkfirewall-firewallpolicy/src/main/java/software/amazon/networkfirewall/firewallpolicy/ClientBuilder.java
+++ aws-networkfirewall-firewallpolicy/src/main/java/software/amazon/networkfirewall/firewallpolicy/ClientBuilder.java
@@ -1,5 +1,6 @@
 package software.amazon.networkfirewall.firewallpolicy;
 
+import java.util.function.Supplier;
 import software.amazon.awssdk.services.networkfirewall.NetworkFirewallClient;
 import software.amazon.cloudformation.LambdaWrapper;
 
@@ -7,6 +8,18 @@ public class ClientBuilder {
 
   public static NetworkFirewallClient getClient() {
     return NetworkFirewallClient.builder()
+            .httpClient(LambdaWrapper.HTTP_CLIENT)
+            .build();
+  }
+
+  public static Supplier<NetworkFirewallClient> getClient(final String region) {
+    if (region.equals("us-gov-west-1") || region.equals("us-gov-east-1")) {
+      return () -> (NetworkFirewallClient) NetworkFirewallClient.builder()
+              .httpClient(LambdaWrapper.HTTP_CLIENT)
+              .fipsEnabled(true))
+              .build();
+    }
+    return () -> (NetworkFirewallClient) NetworkFirewallClient.builder()
             .httpClient(LambdaWrapper.HTTP_CLIENT)
             .build();
   }
